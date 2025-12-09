20720b52faf5f6491ecefc1cad489dc4f1f5a57d
Jet <Yotsanon_aim@cmu.ac.th>
2025-12-09 17:05:18 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    int x=1,a=0,b=0;
+    while(x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if(x%2==0&&x!=0){
+            a++;
+        }
+        else if(x%2!=0&&x!=0){
+            b++;
+        }
+            
+        
+    }
+    cout << "#Even numbers = " << a<<"\n";
+    cout << "#Odd numbers = "<< b;
+   
+ 
+   
 }
